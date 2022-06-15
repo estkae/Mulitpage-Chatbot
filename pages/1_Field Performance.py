b9a5806d08e55dfeeccc66336922ce68d0bdbc36
PrateekKumar2109 <101380064+PrateekKumar2109@users.noreply.github.com>
2022-06-15 19:57:53 +0530

    Update 1_Field Performance.py

--- pages/1_Field Performance.py
+++ pages/1_Field Performance.py
@@ -18,7 +18,7 @@ if data_uploader is not None:
 st.header("The Matser Production Data ")
 st.sidebar.header("User input parameter")
 
-years=st.sidebar.multiselect("Select the year",options=df['YEAR'].unique(),default=data_df['YEAR'].unique())
+years=st.sidebar.multiselect("Select the year",options=data_df['YEAR'].unique(),default=data_df['YEAR'].unique())
 years=np.array(years)
 from datetime import datetime
 #start_time = st.sidebar.slider(
