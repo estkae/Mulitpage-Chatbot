28ba6d35c2e4b4c3afc4db3e38ed22e987950e7c
PrateekKumar2109 <101380064+PrateekKumar2109@users.noreply.github.com>
2022-06-16 21:24:26 +0530

    Update 2_Platform_wise.py

--- pages/2_Platform_wise.py
+++ pages/2_Platform_wise.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
+import plotly.express as px
 
 st.title("""Platform wise Performance Dashboard Page """)
 
