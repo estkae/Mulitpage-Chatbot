7a434d8df1f92acbc3e54a05fbaaaede89b3c1ab
PrateekKumar2109 <101380064+PrateekKumar2109@users.noreply.github.com>
2023-03-18 10:18:45 +0530

    Update 1_Arxiv_papers.py

--- pages/1_Arxiv_papers.py
+++ pages/1_Arxiv_papers.py
@@ -3,31 +3,38 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
+import streamlit as st
+from streamlit_chat import message
+
+
+from langchain.prompts.prompt import PromptTemplate
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import UnstructuredURLLoader
+from langchain.document_loaders import ReadTheDocsLoader
+from langchain.vectorstores.faiss import FAISS
+from langchain.embeddings import OpenAIEmbeddings
+import pickle
+from langchain.chains import ConversationChain
+from langchain.llms import OpenAI,Cohere
+from langchain.vectorstores.faiss import FAISS
+from langchain.embeddings import OpenAIEmbeddings,CohereEmbeddings
+from langchain.chains import ChatVectorDBChain
+import pickle
+from langchain import OpenAI, VectorDBQA
+from langchain.prompts.prompt import PromptTemplate
 import plotly.express as px
 st.title("""Field Level  Performance Dashboard Page """)
 
 st.header("Upload the  Production  data file here ")
 st.markdown(" The file format is  standard Excel File")
 
-data_uploader = st.file_uploader("upload file", type={"csv", "txt",'xlsx'})
-if data_uploader is not None:
-    try:
-          data_df=pd.read_csv(data_uploader)
-          data_df=data_df[['Platform','Well No','Date','Days','YEAR','Ql, blpd', 'Qo, bopd', 'Qw, bopd','RecOil, bbls   ',
-                  'Qg (Assoc. Gas), m3/d','Moil, MMt', 'RecGas, m3']]  
-    except:      
-          data_df=pd.read_excel(data_uploader,sheet_name=10)
-
-          data_df=data_df[['Platform','Well No','Date','Days','YEAR','Ql, blpd', 'Qo, bopd', 'Qw, bopd','RecOil, bbls   ',
-                  'Qg (Assoc. Gas), m3/d','Moil, MMt', 'RecGas, m3']]
-    
-    
+   
 st.header("The  Production Data ")
 st.sidebar.header("User input parameter")
 
 import streamlit as st
 from langchain import PromptTemplate
-from langchain.llms import OpenAI
+from langchain.llms import Cohere
 
 template = """
     Below is an email that may be poorly worded.
@@ -59,12 +66,28 @@ prompt = PromptTemplate(
     template=template,
 )
 
-def load_LLM(openai_api_key):
-    """Logic for loading the chain you want to use should go here."""
-    # Make sure your openai_api_key is set as an environment variable
-    llm = OpenAI(temperature=.7, openai_api_key=openai_api_key)
-    return llm
+def copy_list(a):
+    b = []
+    for i in range(0,4):
+        b.append(a[i].page_content)
+    return b
+def load_vectorstore():
+    '''load embeddings and vectorstore'''
+           
+    embeddings = CohereEmbeddings(cohere_api_key= "vGCEakgncpouo9Nz0rsJ0Bq7XRvwNgTCZMKSohlg")
+       
+    return FAISS.load_local('arxiv_embeddings', embeddings)
 
+def load_data(file_name):
+    """
+    Load a CSV file as a Pandas DataFrame from the data folder in a GitHub repository.
+    """
+    url = "https://raw.githubusercontent.com/PrateekKumar2109/Mulitpage-Chatbot/main/Data/Arxiv_data.csv.csv"
+    
+    df = pd.read_csv(url)
+    return df
+
+df=load_data('arxiv')
 st.set_page_config(page_title="Globalize Email", page_icon=":robot:")
 st.header("Globalize Text")
 
@@ -79,13 +102,9 @@ with col1:
 with col2:
     st.image(image='TweetScreenshot.png', width=500, caption='https://twitter.com/DannyRichman/status/1598254671591723008')
 
-st.markdown("## Enter Your Email To Convert")
+st.markdown("## Enter your Idea to learn")
 
-def get_api_key():
-    input_text = st.text_input(label="OpenAI API Key ",  placeholder="Ex: sk-2twmA8tfCb8un4...", key="openai_api_key_input")
-    return input_text
 
-openai_api_key = get_api_key()
 
 col1, col2 = st.columns(2)
 with col1:
@@ -102,29 +121,37 @@ def get_text():
     input_text = st.text_area(label="Email Input", label_visibility='collapsed', placeholder="Your Email...", key="email_input")
     return input_text
 
-email_input = get_text()
+query= get_text()
 
-if len(email_input.split(" ")) > 700:
+if len(query.split(" ")) > 700:
     st.write("Please enter a shorter email. The maximum length is 700 words.")
     st.stop()
 
-def update_text_with_example():
-    print ("in updated")
-    st.session_state.email_input = "Sally I am starts work at yours monday from dave"
 
 st.button("*See An Example*", type='secondary', help="Click to see an example of the email you will be converting.", on_click=update_text_with_example)
 
 st.markdown("### Your Converted Email:")
 
 if email_input:
-    if not openai_api_key:
-        st.warning('Please insert OpenAI API Key. Instructions [here](https://help.openai.com/en/articles/4936850-where-do-i-find-my-secret-api-key)', icon="⚠️")
-        st.stop()
+    
+    #llm = load_LLM(openai_api_key=openai_api_key)
+    list_queries=docsearch.similarity_search(query)
+    x_page_content=copy_list(list_queries)
+    result = df[df['text_full'].isin(x_page_content)][['title', 'year','authors','abstract']]
+    
+    col1, col2,col3, col4 = st.columns(4)
 
-    llm = load_LLM(openai_api_key=openai_api_key)
+    with col1:
+        st.markdown("Often professionals would like to improve their emails, but don't have the skills to do so. \n\n This tool \
+                will help you improve your email skills by converting your emails into a more professional format. This tool \
+                is powered by [LangChain](https://langchain.com/) and [OpenAI](https://openai.com) and made by \
+                [@GregKamradt](https://twitter.com/GregKamradt). \n\n View Source Code on [Github](https://github.com/gkamradt/globalize-text-streamlit/blob/main/main.py)")
 
-    prompt_with_email = prompt.format(tone=option_tone, dialect=option_dialect, email=email_input)
+    with col2:
+         st.image(image='TweetScreenshot.png', width=500, caption='https://twitter.com/DannyRichman/status/1598254671591723008')
+    
+    #prompt_with_email = prompt.format(tone=option_tone, dialect=option_dialect, email=email_input)
 
-    formatted_email = llm(prompt_with_email)
+    #formatted_email = llm(prompt_with_email)
 
     st.write(formatted_email)
