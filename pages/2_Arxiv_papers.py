2b4958181e541473659e9b8de020b16f34758e80
PrateekKumar2109 <101380064+PrateekKumar2109@users.noreply.github.com>
2023-03-20 10:59:39 +0530

    Update 2_Arxiv_papers.py

--- pages/2_Arxiv_papers.py
+++ pages/2_Arxiv_papers.py
@@ -127,11 +127,7 @@ if query:
 
     #with col1:
          #st.write(result.iloc[0])
-         #st.markdown("Often professionals would like to improve their emails, but don't have the skills to do so. \n\n This tool \
-                will help you improve your email skills by converting your emails into a more professional format. This tool \
-                is powered by [LangChain](https://langchain.com/) and [OpenAI](https://openai.com) and made by \
-                [@GregKamradt](https://twitter.com/GregKamradt). \n\n View Source Code on [Github](https://github.com/gkamradt/globalize-text-streamlit/blob/main/main.py)")
-
+        
     #with col2:
     #     st.write(result.iloc[1])
     
