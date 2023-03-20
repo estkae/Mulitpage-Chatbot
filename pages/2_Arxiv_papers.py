4474634928d7927014209ecac9bd1810a345159f
PrateekKumar2109 <101380064+PrateekKumar2109@users.noreply.github.com>
2023-03-20 10:32:46 +0530

    Update 2_Arxiv_papers.py

--- pages/2_Arxiv_papers.py
+++ pages/2_Arxiv_papers.py
@@ -1,8 +1,6 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import streamlit as st
+
 import streamlit as st
 from streamlit_chat import message
 
